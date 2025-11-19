a2a6b71f026d4389cab9dcdd63bcb240685772ec
Valentina Sandoval <Valen>
2025-11-19 12:04:18 -0500

    Feat(Practica_analitica_POO): Se crearon 4 clases hijas llamadas (Cilindro,Rombo, Paralelograma y Trapecio) estas funciones se agregaron al menu, y estas funciones nos ayudan a calcular el area de un cilindro, un rombo, un paralelograma y un trapecio

--- Menu.py
+++ Menu.py
@@ -3,7 +3,10 @@ from Circulo import Circulo
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
 from Triangulo import Triangulo
-
+from Cilindro import Cilindro
+from Paralelograma import Paralelograma
+from Trapecio import Trapecio
+from Rombo import Rombo
 
 Menu_funciones = True 
 while Menu_funciones:
@@ -12,6 +15,10 @@ while Menu_funciones:
     print("2.Area Cuadrado")
     print("3.Area Rectangulo")
     print("4.Area Triangulo")
+    print("5.Area Cilindro")
+    print("6.Area Paralelograma")
+    print("7.Area Trapecio")
+    print("8.Area Rombo")
     opcion=int(input("Selecciona una opcion: "))
  
        
@@ -40,5 +47,46 @@ while Menu_funciones:
         tr = Triangulo(base , altura)
         resultado = tr.area_triangulo()
         print("El area del triangulo es: ", resultado)
+
+    elif opcion ==5:
+        altura=float(input("Digite un valor de altura: "))
+        radio=float(input("Digite un valor de radio: "))
+        cl = Cilindro("Cilindro")
+        cl.radio = radio
+        cl.altura = altura
+        resultado = cl.area_cilindro()
+        print("El area del cilindro es: ", resultado)
+
+    elif opcion ==6:
+        base=float(input("Digite un valor de base: "))
+        Altura=float(input("Digite un valor de altura: "))
+        pl = Paralelograma("Paralelograma")
+        pl.base = base
+        pl.altura = Altura
+        resultado = pl.area_paralelograma()
+        print("El area del paralelograma es: ", resultado)
+
+    elif opcion ==7:
+        basemayor=float(input("Digite un valor de base mayor: "))
+        basemenor=float(input("Digite un valor de base menor: "))
+        Altura=float(input("Digite un valor de altura: "))
+        tp = Trapecio("Trapecio")
+        tp.baseMayor = basemayor
+        tp.baseMenor = basemenor
+        tp.altura = Altura
+        resultado = tp.area_trapecio()
+        print("El area del trapecio es: ", resultado)
+
+    elif opcion ==8:
+        diagonalmayor=float(input("Digite un valor de diagonal mayor: "))
+        diagonalmenor=float(input("Digite un valor de diagonal menor: "))
+        rb = Rombo("Rombo")
+        rb.diagonalMayor = diagonalmayor
+        rb.diagonalMenor = diagonalmenor
+        resultado = rb.area_rombo()
+        print("El area del rombo es: ", resultado)
+
+
+
 
     
